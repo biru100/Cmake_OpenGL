86667e0fe65a5d42c6ef89f9b73e3a9ec0a1535d
To_Ran <41686179+biru100@users.noreply.github.com>
2020-01-16 03:21:56 +0900

    Set CMake Setting

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -1,12 +1,9 @@
 ﻿// CMake_OpenGL.cpp : Defines the entry point for the application.
 //
 
-#include "CMake_OpenGL.h"
-
-using namespace std;
+#include <iostream>
 
 int main()
 {
-	cout << "Hello CMake." << endl;
 	return 0;
 }
