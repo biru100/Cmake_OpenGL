8093516138acabec7e55707f0b874a915ee00a45
To_Ran <41686179+biru100@users.noreply.github.com>
2020-01-15 21:38:52 +0900

    Set CMake

---
+++ CMake_OpenGL.cpp
@@ -0,0 +1,12 @@
+﻿// CMake_OpenGL.cpp : Defines the entry point for the application.
+//
+
+#include "CMake_OpenGL.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
